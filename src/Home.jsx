f2513abf28e18372ce96576059feb99a354bbabf
yennamsreekeerthi-reddy <yennamsreekeerthi@gmail.com>
2026-02-26 11:57:39 +0530

    Initial commit – react router setup

---
+++ src/Home.jsx
@@ -0,0 +1,9 @@
+function Home()
+{
+    return (
+        <>
+            <h1>Iam Home...</h1>
+        </>
+    )
+}
+export default Home;
---
+++ src/Reg.jsx
@@ -0,0 +1,43 @@
+ import axios from "axios"; 
+import { useState } from "react";
+function Reg()
+{
+    const [data,setData]=useState(
+        {
+            username:"",
+            email:"",
+            password:""     
+        }
+    )
+    const changeName=(e)=>
+    {
+        setData({...data,[e.target.name]:e.target.value})
+    }
+    const submit=async()=>
+    {
+        try
+        {
+            //const res=await axiox.post("http://localhost:5000/reg",data)
+            const res=await axios.post(
+                "http://localhost:8080/reg",
+                data
+            )
+            alert(res.data)
+        }
+        catch(xyz)
+        {
+            //alert(xyz.response.data)
+            alert(xyz.response?.data||"Error")
+        }
+    }
+    return(
+        <>
+            <h1>Iam App...</h1>
+            <input onChange={changeName} name="username" placeholder="Username"/><br/>
+            <input onChange={changeName} name="email" placeholder="Email"/><br/>
+            <input onChange={changeName} name="password" placeholder="Password"/><br/>
+            <button onClick={submit}>register</button>
+        </>
+    )
+}
+export default Reg;
